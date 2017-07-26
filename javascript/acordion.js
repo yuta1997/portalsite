6af4585071d0589ea1aca44e8909767fe5051b1e
yuta1997 <zakkoku429@yahoo.co.jp>
2017-07-26 15:33:18 +0900

    yuta

---
+++ javascript/acordion.js
@@ -0,0 +1,36 @@
+
+	$(document).ready(function(){
+		$("div#item_a").click(function(){
+			$(this).next().slideToggle(300);
+		});
+	});
+
+	$(document).ready(function(){
+		$("div#item_b").click(function(){
+			$(this).next().slideToggle(300);	
+		});
+	});
+
+	$(document).ready(function(){
+		$("div#item_c").click(function(){
+			$(this).next().slideToggle(300);
+		});
+	});
+
+	$(document).ready(function(){
+		$("div#item_teach_a").click(function(){
+			$(this).next().slideToggle(300);
+		});
+	});
+
+	$(document).ready(function(){
+		$("div#item_teach_b").click(function(){
+			$(this).next().slideToggle(300);
+		});
+	});
+
+	$(document).ready(function(){
+		$("div#item_teach_c").click(function(){
+			$(this).next().slideToggle(300);
+		});
+	});
