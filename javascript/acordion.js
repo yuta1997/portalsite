6bb4e1fa700e16019bf506052fd7fc9ada4c14d2
yuta1997 <zakkoku429@yahoo.co.jp>
2017-07-27 17:30:23 +0900

    yuta

--- javascript/acordion.js
+++ javascript/acordion.js
@@ -6,10 +6,12 @@
         $("div#container_semi").each(function() {
             var accordion = $(this);
             $(this).find("div#item_semi").click(function() {
+                $("div#item_semi img").removeClass("rotare")
                 var targetContentWrap = $(this).next("div#contents_semi");
                 if ( targetContentWrap.css("display") === "none" ) {
                     accordion.find("div#contents_semi").slideUp();
                     accordion.find("div#item_semi.div#contents_semi").removeClass("open");
+                    $("div#item_semi img",this).addClass("rotare");
                 }
                 targetContentWrap.slideToggle();
                 $(this).toggleClass("open");
@@ -25,13 +27,15 @@
         $("div#container_teach").each(function() {
             var accordion = $(this);
             $(this).find("div#item_teach").click(function() {
+                $("div#item_teach img").removeClass('rotare')
                 var targetContentWrap = $(this).next("div#contents_teach");
                 if ( targetContentWrap.css("display") === "none" ) {
                     accordion.find("div#contents_teach").slideUp();
-                    accordion.find("div#item_teach.div#contents_teach").removeClass("open");
+                    accordion.find("div#item_teach.div#contents_teach").removeClass('open');
+                    $("div#item_teach img",this).addClass('rotare');
                 }
                 targetContentWrap.slideToggle();
-                $(this).toggleClass("open");
+                $(this).toggleClass('open');
             });
         });
     
