1d0c163817995af8256a003f45527897884113bc
yuta1997 <zakkoku429@yahoo.co.jp>
2017-07-27 15:50:58 +0900

    yuta

--- javascript/acordion.js
+++ javascript/acordion.js
@@ -1,36 +1,41 @@
 
-	$(document).ready(function(){
-		$("div#item_a").click(function(){
-			$(this).next().slideToggle(300);
-		});
-	});
+	//ゼミ情報のアコーディオン	
+	$(function() {
+    	$("div#contents_semi").hide();
+        // アコーディオン
+        $("div#container_semi").each(function() {
+            var accordion = $(this);
+            $(this).find("div#item_semi").click(function() {
+                var targetContentWrap = $(this).next("div#contents_semi");
+                if ( targetContentWrap.css("display") === "none" ) {
+                    accordion.find("div#contents_semi").slideUp();
+                    accordion.find("div#item_semi.div#contents_semi").removeClass("open");
+                }
+                targetContentWrap.slideToggle();
+                $(this).toggleClass("open");
+            });
+        });
+    
+    });
 
-	$(document).ready(function(){
-		$("div#item_b").click(function(){
-			$(this).next().slideToggle(300);	
-		});
-	});
+	//レポートのアコーディオン
+	$(function() {
+    	$("div#contents_teach").hide();
+        // アコーディオン
+        $("div#container_teach").each(function() {
+            var accordion = $(this);
+            $(this).find("div#item_teach").click(function() {
+                var targetContentWrap = $(this).next("div#contents_teach");
+                if ( targetContentWrap.css("display") === "none" ) {
+                    accordion.find("div#contents_teach").slideUp();
+                    accordion.find("div#item_teach.div#contents_teach").removeClass("open");
+                }
+                targetContentWrap.slideToggle();
+                $(this).toggleClass("open");
+            });
+        });
+    
+    });
 
-	$(document).ready(function(){
-		$("div#item_c").click(function(){
-			$(this).next().slideToggle(300);
-		});
-	});
 
-	$(document).ready(function(){
-		$("div#item_teach_a").click(function(){
-			$(this).next().slideToggle(300);
-		});
-	});
-
-	$(document).ready(function(){
-		$("div#item_teach_b").click(function(){
-			$(this).next().slideToggle(300);
-		});
-	});
-
-	$(document).ready(function(){
-		$("div#item_teach_c").click(function(){
-			$(this).next().slideToggle(300);
-		});
-	});
+	
