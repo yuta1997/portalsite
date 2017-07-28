83af50c2db4dc03811f9fead5eb6de333648b59a
yuta1997 <zakkoku429@yahoo.co.jp>
2017-07-28 09:38:17 +0900

    yuta

--- javascript/acordion.js
+++ javascript/acordion.js
@@ -27,12 +27,12 @@
         $("div#container_teach").each(function() {
             var accordion = $(this);
             $(this).find("div#item_teach").click(function() {
-                $("div#item_teach img").removeClass('rotare')
+                $('arrow').removeClass('rotare')
                 var targetContentWrap = $(this).next("div#contents_teach");
                 if ( targetContentWrap.css("display") === "none" ) {
                     accordion.find("div#contents_teach").slideUp();
                     accordion.find("div#item_teach.div#contents_teach").removeClass('open');
-                    $("div#item_teach img",this).addClass('rotare');
+                    $('arrow').addClass('rotare');
                 }
                 targetContentWrap.slideToggle();
                 $(this).toggleClass('open');
